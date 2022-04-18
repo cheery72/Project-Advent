027a557a38ad43fdcba10b31b674bd19fcc01c3e
문찬송 <moon.sw.good@gmail.com>
2022-04-18 18:06:03 +0900

    feat: FE main_navbar 틀 작성

--- frontend/src/component/navbar.tsx
+++ frontend/src/component/navbar.tsx
@@ -1,58 +1,98 @@
-import { useRouter } from 'next/router';
-import { SetStateAction, useState } from 'react';
-import { Menu } from 'semantic-ui-react'
+import Link from 'next/link';
+// import { useRouter } from 'next/router';
+// import { SetStateAction, useState } from 'react';
+// import { Menu } from 'semantic-ui-react'
+import styles from '../../styles/navbar/navbar.module.css';
 
 export default function Navbar(){
 
-    const router = useRouter()
-    const [activeItem, setActivItem] = useState('home')
+    // const router = useRouter()
+    // const [activeItem, setActivItem] = useState('home')
 
-    function changeActive(now: SetStateAction<string>){
-        setActivItem(now)
-    }
+    // function changeActive(now: SetStateAction<string>){
+    //     setActivItem(now)
+    // }
 
-    function move(url: string){
-        router.push(`/${url}`)
-    }
+    // function move(url: string){
+    //     router.push(`/${url}`)
+    // }
 
     return(
-        <>
-            <Menu>
-                <Menu.Item
-                name='home'
-                active={activeItem === 'home'}
-                onClick={()=>{changeActive('home'), move('/')}}
-                >
-                Home
-                </Menu.Item>
+        <nav className={ styles.menuContainer }>
+            {/* burger menu */}
+            <input type="checkbox" aria-label="Toggle menu" />
+            <span></span>
+            <span></span>
+            <span></span>
 
-                <Menu.Item
-                name='write'
-                active={activeItem === 'write'}
-                onClick={()=>{changeActive('write'), move('write')}}
-                >
-                작성하기
-                </Menu.Item>
+            {/* logo */}
+            <a href="/" className={ styles.menuLogo }>
+                <img src="/temp_logo.png" alt="Advent Special Day"/>
+            </a>
 
-                <Menu.Item
-                name='sendbox'
-                active={activeItem === 'sendbox'}
-                onClick={()=>{changeActive('sendbox'), move('sendbox')}}
-                >
-                작성 선물함
-                </Menu.Item>
+            {/* menu items */}
+            <div className={ styles.menu }>
+                <ul>
+                    <li>
+                        <Link href="/write">
+                            선물 작성하기
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/sendbox">
+                            보낸 선물함
+                        </Link>
+                    </li>
+                    </ul>
+                    <ul>
+                    <li>
+                        <a href="/signin">
+                            카카오 로그인
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 
-                <Menu.Menu position='right'>
-                <Menu.Item
-                    name='signin'
-                    active={activeItem === 'signin'}
-                    onClick={()=>{changeActive('signin')}}
-                >
-                    로그인
-                </Menu.Item>
+    // return(
+    //     <>
+    //         <Menu>
+    //             <Menu.Item
+    //             name='home'
+    //             active={activeItem === 'home'}
+    //             onClick={()=>{changeActive('home'), move('/')}}
+    //             >
+    //             Home
+    //             </Menu.Item>
 
-                </Menu.Menu>
-            </Menu>
-        </>
-    );
+    //             <Menu.Item
+    //             name='write'
+    //             active={activeItem === 'write'}
+    //             onClick={()=>{changeActive('write'), move('write')}}
+    //             >
+    //             작성하기
+    //             </Menu.Item>
+
+    //             <Menu.Item
+    //             name='sendbox'
+    //             active={activeItem === 'sendbox'}
+    //             onClick={()=>{changeActive('sendbox'), move('sendbox')}}
+    //             >
+    //             작성 선물함
+    //             </Menu.Item>
+
+    //             <Menu.Menu position='right'>
+    //             <Menu.Item
+    //                 name='signin'
+    //                 active={activeItem === 'signin'}
+    //                 onClick={()=>{changeActive('signin')}}
+    //             >
+    //                 로그인
+    //             </Menu.Item>
+
+    //             </Menu.Menu>
+    //         </Menu>
+    //     </>
+    // );
 }
