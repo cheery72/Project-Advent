096da0fa52821e779b71aa716f77161931e9904d
문찬송 <moon.sw.good@gmail.com>
2022-04-21 13:18:25 +0900

    feat: FE main_index 비로그인/로그인시 메인페이지 틀 추가

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,43 +1,78 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Button, Grid, Header } from 'semantic-ui-react'
+import styles from '../styles/index/index.module.css'
+import { Button, Grid, Header, Icon } from 'semantic-ui-react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+const { Row, Column } = Grid
 
 const Home: NextPage = () => {
   
   const router = useRouter()
 
-  function goWrite(){
+  const KAKAO_LOGIN_URL = 'http://kwky.shop:8081'
+  const BASE_URL = 'http://localhost:3000'
+
+  const [isLogined, setIsLogin] = useState<boolean>(false)
+
+  useEffect(() => {
+      if (localStorage.token) { 
+          setIsLogin(true)     
+      } else {
+          setIsLogin(false)
+      }    
+  }, []);
+
+  const goWrite = () => {
     router.push('/write')
   }
 
   return (
     <>
-      <Grid>
-        <Grid.Row />
-        <Grid.Row>
-          <Grid.Column width={5}/>
-          <Grid.Column width={6}>
-            <Header as='h1' textAlign='center'>Make Our Special</Header>
-            <Header as='h1' textAlign='center'>어드벤트 스페셜 데이</Header>
-          </Grid.Column>
-          <Grid.Column width={5} />
-        </Grid.Row>
-
-        <Grid.Row textAlign='center'>
-          <Grid.Column width={3}/>
-          <Grid.Column width={5}>
-            <p>이미지</p>
-          </Grid.Column>
-          <Grid.Column width={5}>
-            <p>로그인 창</p>
-            <p>로그인 했다고 가정</p>
-            <Button onClick={goWrite}>작성하기</Button>
-          </Grid.Column>
-          <Grid.Column width={3}/>
-        </Grid.Row>
+      <div className={ styles.titleWrapper }>
+        <h1 className={ styles.titleStyle }>Make Our Special</h1>
+        <h1 className={ styles.titleStyle }>어드벤트 스페셜 데이</h1>
+      </div>  
+      <Grid centerd>
+        <Row textAlign='center'>
+          <Column width={3} />
+          <Column width={5}>
+            <img src='/main/temp_main.png' />
+          </Column>
+          <Column width={5} className={ styles.mainBox }>
+            
+            <h2 className={ styles.mainText }>소중한 사람에게 <br /> 특별한 선물을 해보세요.</h2>
+            {/* 로그인 유무 판별 */}
+            {
+              isLogined ?
+              <Button 
+                color='twitter' 
+                animated 
+                onClick={() => goWrite()}
+                // className={ styles.writeButton }
+                style={{ height:'18%', width:'63%', padding:'5.5%', fontSize:'1.5vw' }}
+              >
+                <Button.Content visible>
+                  <Icon name='gift' color='yellow'/>선물하러 가기!
+                </Button.Content>
+                <Button.Content hidden>
+                  <Icon name='arrow right' />Go!
+                </Button.Content>
+              </Button>
+              : 
+              <Link href={`${KAKAO_LOGIN_URL}/oauth2/authorization/kakao?redirect_uri=${BASE_URL}/oauth/redirect`}>
+                  <img 
+                      src="/kakao_button/kakao_login_large_narrow.png" 
+                      className={ styles.kakaoButton }       
+                      alt="카카오로그인"
+                  />
+              </Link>
+            }
+          </Column>
+          <Column width={3}/>
+        </Row>
       </Grid>
     </>
   )
