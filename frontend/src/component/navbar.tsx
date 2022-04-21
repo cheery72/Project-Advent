4432bbc30ebfc8b6a3fa67470593192b8ee99ee3
문찬송 <moon.sw.good@gmail.com>
2022-04-21 22:15:43 +0900

    fix: FE main_navbar hamburger X버튼 css 에러 수정

--- frontend/src/component/navbar.tsx
+++ frontend/src/component/navbar.tsx
@@ -26,71 +26,73 @@ export default function Navbar() {
     }
     
     return(
-        <nav className={ styles.menuContainer }>
-            <ToastContainer autoClose={1000} /> {/* react tostify 알람 모달 -> 하나만 있어도 됨 */} 
+        <>
+            <ToastContainer autoClose={1000} />
+            <nav className={ styles.menuContainer }>
 
-            {/* burger menu */}
-            <input type="checkbox" aria-label="Toggle menu" />
-            <span></span>
-            <span></span>
-            <span></span>
+                {/* burger menu */}
+                <input type="checkbox" aria-label="Toggle menu" />
+                <span></span>
+                <span></span>
+                <span></span>
 
-            {/* logo */}
-            <Link href="/">
-                <a className={ styles.menuLogo }> {/* style 적용때문에 a태그 사용, Link태그 대신 a태그만 사용하면 페이지 이동시 rerendering 함*/}
-                    <img src="/temp_logo.png" alt="Advent Special Day"  />
-                </a>
-            </Link>
+                {/* logo */}
+                <Link href="/">
+                    <a className={ styles.menuLogo }> {/* style 적용때문에 a태그 사용, Link태그 대신 a태그만 사용하면 페이지 이동시 rerendering 함*/}
+                        <img src="/temp_logo.png" alt="Advent Special Day"  />
+                    </a>
+                </Link>
 
-            {/* menu items */}
-            <div className={ styles.menu }>     
-                <ul>
-                    {
-                        IsLogin() && 
-                        <>
-                            <li>
-                                <Link href="/write">
-                                    선물 작성하기
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/sendbox">  
-                                    보낸 선물함
-                                </Link>
-                            </li>
-                        </>
-                    }
-                </ul>
-                <ul>
-                    <li>
+                {/* menu items */}
+                <div className={ styles.menu }>     
+                    <ul>
                         {
-                            IsLogin() ? 
-                            <Button 
-                                animated
-                                color="yellow"
-                                size="large"
-                                onClick={() => logout()}
-                            >
-                                <Button.Content visible>
-                                    <Icon name="sign-out" />Logout
-                                </Button.Content>
-                                <Button.Content hidden>
-                                    <Icon color="red" name="heart" />See you!
-                                </Button.Content>
-                            </Button>
-                            :
-                            // 임시경로( 소셜로그인테스트: kwky.shop:8081 / 리다이렉트: localhost:3000 )
-                            <Link href={`${KAKAO_LOGIN_URL}/oauth2/authorization/kakao?redirect_uri=${BASE_URL}/oauth/redirect`}>
-                                <img 
-                                    src="/kakao_button/kakao_login_large.png" 
-                                    className={ styles.kakaoButton }       
-                                    alt="카카오로그인"
-                                />
-                            </Link>
+                            IsLogin() && 
+                            <>
+                                <li>
+                                    <Link href="/write">
+                                        선물 작성하기
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/sendbox">  
+                                        보낸 선물함
+                                    </Link>
+                                </li>
+                            </>
                         }
-                    </li>
-                </ul>
-            </div>
-        </nav>
+                    </ul>
+                    <ul>
+                        <li>
+                            {
+                                IsLogin() ? 
+                                <Button 
+                                    animated
+                                    color="yellow"
+                                    size="large"
+                                    onClick={() => logout()}
+                                >
+                                    <Button.Content visible>
+                                        <Icon name="sign-out" />Logout
+                                    </Button.Content>
+                                    <Button.Content hidden>
+                                        <Icon color="red" name="heart" />See you!
+                                    </Button.Content>
+                                </Button>
+                                :
+                                // 임시경로( 소셜로그인테스트: kwky.shop:8081 / 리다이렉트: localhost:3000 )
+                                <Link href={`${KAKAO_LOGIN_URL}/oauth2/authorization/kakao?redirect_uri=${BASE_URL}/oauth/redirect`}>
+                                    <img 
+                                        src="/kakao_button/kakao_login_large.png" 
+                                        className={ styles.kakaoButton }       
+                                        alt="카카오로그인"
+                                    />
+                                </Link>
+                            }
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
     )
 }
