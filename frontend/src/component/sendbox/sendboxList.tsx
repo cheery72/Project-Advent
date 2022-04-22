4bbf3948940d64f323c3d32b63d6e5f55fabe613
문찬송 <moon.sw.good@gmail.com>
2022-04-22 17:05:11 +0900

    chore: FE sendbox 데이터테스트 및 배치 적용

--- frontend/src/component/sendbox/sendboxList.tsx
+++ frontend/src/component/sendbox/sendboxList.tsx
@@ -4,7 +4,27 @@ import SendboxListItem from "./sendboxListItem";
 const { Row, Column } = Grid
 
 export default function sendboxList(){
+    // 테스트용 임시데이터
+    const items = [
+        {id:1, isSubmitted:false, presentTitle:'지애님께 보내는 선물', dDay:'2022-04-30'},
+        {id:2, isSubmitted:false, presentTitle:'흔들리는 꽃들 속 에서 네 샴푸 향이 느껴진 거야', dDay:'2022-04-23'},
+        {id:3, isSubmitted:false, presentTitle:'My precious💍', dDay:'2022-04-24'},
+        {id:4, isSubmitted:true, presentTitle:'6팀 화이팅~🎇', dDay:'2022-04-25'},
+        {id:5, isSubmitted:true, presentTitle:'7팀 화이팅~🎇', dDay:'2022-04-25'},
+        {id:6, isSubmitted:true, presentTitle:'8팀 화이팅~🎇', dDay:'2022-04-25'},
+    ]
+    
     return (
-        <SendboxListItem />
+        <div style={{margin:'30px 10%'}}>
+            <Grid columns={2}>
+                {items.map((item:any) => 
+                    <Column>
+                        <Row width={8}>
+                            <SendboxListItem key={item.id} item={item} />
+                        </Row> 
+                    </Column>)
+                }
+            </Grid>
+        </div>
     )
 }
--- frontend/src/component/sendbox/sendboxListItem.tsx
+++ frontend/src/component/sendbox/sendboxListItem.tsx
@@ -5,29 +5,40 @@ import styles from '../../../styles/sendbox/sendboxListItem.module.css'
 
 const { Row, Column } = Grid
 
-export default function SendboxListItem(){
-    const item = {isSubmitted:false, presentTitle:'OO님께 보내는 선물', dDay:'2022-04-30'}
+export default function SendboxListItem({item}:any){
+
+    const { isSubmitted, dDay, presentTitle } = item
 
     // 오늘 날짜 : d-day 계산
+    let today = new Date();   
+
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1;  // 월
+    let date = today.getDate();  // 날짜
+    
+    // 같은 달 현재 이후 날짜 기준으로만 임시 구현 : d-day가 다른 달인 경우에는 이후에 추가 구현
+    const dDayCount = () => {
+        return Number(item.dDay.substr(8)) - date
+    }
 
 
     return (
-        <Grid columns={3} style={{boxSizing: 'border-box', width:'500px', height:'230px', backgroundColor:'AliceBlue', borderRadius:'10px', boxShadow:'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px', padding:'15px 15px', marginLeft:'20px'}}>
+        <Grid columns={3} style={{boxSizing: 'border-box', width:'100%', height:'100%', backgroundColor:'AliceBlue', borderRadius:'10px', boxShadow:'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px', padding:'20px 10px', margin:'0 auto'}}>
             <Row>
                 <Column width={5}>
                     <Image src="/sendbox/temp_sendbox_img.png" size='medium' wrapped />
                 </Column>
                 <Column width={8}>
                     { 
-                        item.isSubmitted ? 
+                        isSubmitted ? 
                         <Icon circular name='gift' size='large' inverted color='purple' style={{marginBottom:'10px'}}/>
                         :
                         <Icon circular name='gift' size='large' inverted color='yellow' style={{marginBottom:'10px'}}/>
                     }
                     <br />
-                    <Link href=''><a className={styles.title}>❝{item.presentTitle}❞</a></Link>
+                    <Link href=''><a className={styles.title}>❝{presentTitle}❞</a></Link>
                 </Column>
-                { !item.isSubmitted && // 전달 전에만 수정, 삭제가 가능
+                { !isSubmitted && // 전달 전에만 수정, 삭제가 가능
                 ( <Column width={3}>
                     <Button animated='fade' color='blue' style={{margin:'10px 0'}}>
                         <Button.Content hidden>수정</Button.Content>
@@ -50,15 +61,15 @@ export default function SendboxListItem(){
                     item.isSubmitted ? 
                         <></>
                         :
-                        <Button color='yellow' style={{color:'black', background:'#F9E84F'}}>
+                        <Button style={{ color:'black', background:'#F9E84F' }}> {/* hover설정 별도로 주기 */}
                             <Icon name='comment' />전달하기
                         </Button>
                 }
                 </Column>
                 <Column width={7} />
                 <Column width={3} textAlign='center'>
-                    <p style={{ boxSizing: 'border-box', height:'30px', padding:'4px', borderRadius: '100%', backgroundColor: 'white', fontWeight: 'bold', boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;'}}>
-                        {item.isSubmitted ? '보냄' : 'D - 1'}
+                    <p style={{ boxSizing: 'border-box', height:'30px', padding:'4px', borderRadius: '100%', backgroundColor: 'white', fontWeight: 'bold', boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'}}>
+                        {item.isSubmitted ? '보냄' : `D - ${dDayCount()}`}
                     </p>
                 </Column>
             </Row>
