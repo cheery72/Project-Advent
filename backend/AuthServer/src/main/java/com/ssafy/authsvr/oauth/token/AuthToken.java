14920c587790d18a55e5847e9a901a9c6e914bf8
오윤진 <funnyjin710@gmail.com>
2022-04-25 17:26:50 +0900

    refactor: BE AuthToken 수정

--- backend/AuthServer/src/main/java/com/ssafy/authsvr/oauth/token/AuthToken.java
+++ backend/AuthServer/src/main/java/com/ssafy/authsvr/oauth/token/AuthToken.java
@@ -10,10 +10,10 @@ import java.util.Date;
 @Slf4j
 @RequiredArgsConstructor
 public class AuthToken {
-
     @Getter
     private final String token;
     private final Key key;
+
 
     private static final String AUTHORITIES_KEY = "role";
 
@@ -25,6 +25,10 @@ public class AuthToken {
     AuthToken(String id, String role, Date expiry, Key key) {
         this.key = key;
         this.token = createAuthToken(id, role, expiry);
+    }
+
+    public boolean validate() {
+        return this.getTokenClaims() != null;
     }
 
     private String createAuthToken(String id, Date expiry) {
@@ -42,10 +46,6 @@ public class AuthToken {
                 .signWith(key, SignatureAlgorithm.HS256)
                 .setExpiration(expiry)
                 .compact();
-    }
-
-    public boolean validate() {
-        return this.getTokenClaims() != null;
     }
 
     public Claims getTokenClaims() {
