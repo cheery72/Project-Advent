dda353cd05eef4673c6298b6fce5e1b74ea07328
문찬송 <moon.sw.good@gmail.com>
2022-04-26 16:08:31 +0900

    fix: FE main builderr 및 css 수정

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import styles from '../styles/index/index.module.css'
-import { Button, Grid, Icon } from 'semantic-ui-react'
+import { Button, Grid, Icon, Image } from 'semantic-ui-react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import IsLogin from '../src/lib/IsLogin'
+import Head from 'next/head'
+// import Image from 'next/image'
 
 const { Row, Column } = Grid
 
@@ -18,10 +20,13 @@ const Home: NextPage = () => {
     }
 
     return (
-        <div className={ styles.background }>
+        <div>
+            <Head>
+                <title> Make Our Special </title>
+            </Head>
             <div className={ styles.titleWrapper } 
-                data-aos="fade-up" 
-                data-aos-duration="3000"
+                data-aos='fade-up' 
+                data-aos-duration='3000'
             >
                 <h2 className={ `${styles.titleStyle}` }>
                 <span className={ `${styles.titleWord} ${styles.titleWord1}` }>Make&nbsp;</span>
@@ -35,14 +40,17 @@ const Home: NextPage = () => {
                 <Row textAlign='center'>
                     <Column width={3} />
                     <Column width={5} 
-                        data-aos="zoom-in-right" 
-                        data-aos-duration="3000"
+                        data-aos='zoom-in-right' 
+                        data-aos-duration='3000'
                     >
-                        <img src='/main/temp_main.png' />
+                        <Image
+                            src='/main/temp_main.png' 
+                            alt='어드벤트 스페셜 데이'
+                        />
                     </Column>
                     <Column width={5} 
                         className={ styles.mainCard } 
-                        data-aos="flip-left"
+                        data-aos='flip-left'
                         data-aos-duration="3000"
                     >
                         <h2 className={ styles.mainText }>소중한 사람에게 <br />특별한 선물을 해보세요</h2> 
@@ -56,24 +64,28 @@ const Home: NextPage = () => {
                                 style={{ height:'18%', width:'63%', padding:'5.5%', fontSize:'1.5vw' }}
                             >
                                 <Button.Content visible>
-                                <Icon name='gift' color='yellow'/>선물하러 가기!
+                                    <Icon name='gift' color='yellow'/>선물하러 가기!
                                 </Button.Content>
                                 <Button.Content hidden>
-                                <Icon name='arrow right' />Go!
+                                    <Icon name='arrow right' />Go!
                                 </Button.Content>
                             </Button>
                             : 
-                            <Link href={`${KAKAO_LOGIN_URL}/oauth2/authorization/kakao?redirect_uri=${BASE_URL}/oauth/redirect`}>
-                                <img 
-                                    src="/kakao_button/kakao_login_large_narrow.png" 
-                                    className={ styles.kakaoButton }       
-                                    alt="카카오로그인"
+                            <Link 
+                                href={`${KAKAO_LOGIN_URL}/oauth2/authorization/kakao?redirect_uri=${BASE_URL}/oauth/redirect`}
+                                passHref
+                            >
+                                <Image
+                                    src='/kakao_button/kakao_login_large_narrow.png' 
+                                    className={ styles.kakaoButton }   
+                                    alt='카카오로그인'
                                 />
                             </Link>
                         }
                     </Column>
                     <Column width={3}/>
                 </Row>
+                <Row />
             </Grid>
         </div>
     )
