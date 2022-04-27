d5b8a40f95341d362f2bd1b909ac0b19095af8f5
김경민 <nutmy52@naver.com>
2022-04-27 16:32:40 +0900

    fix: cors-setting

--- backend/AuthServer/src/main/java/com/ssafy/authsvr/service/UserServiceImpl.java
+++ backend/AuthServer/src/main/java/com/ssafy/authsvr/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.ssafy.authsvr.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDate;
 import java.util.NoSuchElementException;
 import java.util.Optional;
