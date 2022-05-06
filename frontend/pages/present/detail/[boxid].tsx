0382932ac78e1a25a0e8098d7706f464899c4cc3
문찬송 <moon.sw.good@gmail.com>
2022-05-06 16:56:40 +0900

    feat: FE present_box axios 적용(임시)

--- frontend/pages/present/detail/[boxid].tsx
+++ frontend/pages/present/detail/[boxid].tsx
@@ -1,30 +1,52 @@
-import { Grid, Button, Advertisement } from "semantic-ui-react";
-import React from 'react'
+import { Grid, Button, Image } from "semantic-ui-react";
+import React, { useEffect, useState } from 'react'
 import { useRouter } from "next/router";
 
 import styles from "../../../styles/detail/detail.module.css"
+import allAxios from "../../../src/lib/allAxios";
 
 export default function Presentdetail(){
     const router = useRouter();
     const {Row, Column} = Grid
+    const boxId = router.query.boxid
+    const [content, setContent] = useState('/main/temp_main.png') // 기본이미지(임시)
+    const [day, setDay] = useState('Day')
+
+    const loadIsPassword = async () => {
+        await allAxios
+            .get(`/boxes/${boxId}`)
+            .then(({ data }) => {
+                console.log(data)
+                if (data.content) {
+                    setContent(data.content)
+                    setDay(data.advent_day)
+                }
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    }
+
+    useEffect(() => {
+        loadIsPassword()
+    }, [])
 
 return(
     <>
         <div className={styles.presentdetailhead}>
-            D-7
+            <span>✨&nbsp;D-{day}&nbsp;✨</span>
         </div>
         <Grid stackable>
         <Row>
             <Column width={4}></Column>
             <Column width={8}>
                 <div className={styles.boxlocation}>
-                    <div className={styles.box}>
-                    </div>
+                    <Image src={content} alt="present_image" className={styles.box} />
                 </div>
             </Column>
             <Column width={4}>
                 <div className={styles.buttonbetween}>
-                    <Button inverted color='blue' onClick={() => {router.push(`/present/presentid`);}}>뒤로 가기</Button>
+                    <Button inverted color='blue' onClick={() => {router.back();}}>뒤로 가기</Button>
                 </div>        
             </Column>
         </Row>
