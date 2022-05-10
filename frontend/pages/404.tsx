2149da624332e966d0d7f1247231f5ee44bbb3ba
문찬송 <moon.sw.good@gmail.com>
2022-05-10 16:14:16 +0900

    feat: FE errorpage 404페이지 디자인 반영

---
+++ frontend/pages/404.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { Header, Image } from "semantic-ui-react";
+import styles from '../styles/404/404.module.css'
+
+export default function Custom404() {
+  return (
+    <>
+        <Head>
+            <title>404: Page Not Found | Make Our Special</title>
+        </Head>
+        <Header as='h1' textAlign="center" style={{marginBottom:'30px'}}>
+          <span className={ styles.title }>404 &nbsp;|&nbsp; Page Not Found</span>
+        </Header>
+        <Image 
+            src="/404/box-empty.png" alt="404_page_image" 
+            size="medium"
+            centered
+        />
+    </>
+  )
+}
