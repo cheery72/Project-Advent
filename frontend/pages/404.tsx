ca7e7659af67dc2fba521e9b69a045447886f12e
문찬송 <moon.sw.good@gmail.com>
2022-05-10 16:18:05 +0900

    chore: FE errorpage 404 색상 수정

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -8,11 +8,16 @@ export default function Custom404() {
         <Head>
             <title>404: Page Not Found | Make Our Special</title>
         </Head>
-        <Header as='h1' textAlign="center" style={{marginBottom:'30px'}}>
+        <Header 
+            as='h1'
+            textAlign="center" 
+            style={{marginBottom:'30px'}}
+        >
           <span className={ styles.title }>404 &nbsp;|&nbsp; Page Not Found</span>
         </Header>
         <Image 
-            src="/404/box-empty.png" alt="404_page_image" 
+            src="/404/box-empty.png" 
+            alt="404_page_image" 
             size="medium"
             centered
         />
