756af3a8cb69ee8e76053207070795e8ee90f5b5
오윤진 <funnyjin710@gmail.com>
2022-05-10 23:39:37 +0900

    comment: BE eurekaserver 접속포트 표시

--- backend/EurekaServer/src/main/java/com/ssafy/eurekasvr/EurekaServerApplication.java
+++ backend/EurekaServer/src/main/java/com/ssafy/eurekasvr/EurekaServerApplication.java
@@ -8,8 +8,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerApplication {
 
+	//http://k6c206.p.ssafy.io:8761/ : spring Eureka 접속
 	public static void main(String[] args) {
+		
 		SpringApplication.run(EurekaServerApplication.class, args);
+		
 	}
 
 }
