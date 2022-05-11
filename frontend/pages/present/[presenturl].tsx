0d1aef7cd3cbc294b048683c51be0f689010ecb5
문찬송 <moon.sw.good@gmail.com>
2022-05-11 11:54:06 +0900

    feat: FE present 공유링크 metatag 반영

--- frontend/pages/present/[presenturl].tsx
+++ frontend/pages/present/[presenturl].tsx
@@ -8,24 +8,24 @@ export default function Present({title, thumbnail}:any){
         <>
             <Head>
                 <title>선물 확인하기 | Make Our Special</title>
-                <meta property="og:title" content={ title ? title : "Advent Special Day" }/>
+                <meta property="og:title" content={ title ? title : "선물이 도착했습니다." }/>
                 <meta property="og:description" content="선물 확인하기 | Make Our Special" />
-                <meta property="og:image" content={thumbnail ? thumbnail : "https://cdn.icon-icons.com/icons2/510/PNG/512/at_icon-icons.com_50456.png"} /> {/* 임시이미지--수정해야함 */}
+                <meta property="og:image" content={thumbnail ? thumbnail : "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/62b91d44-d9a4-4e80-8043-47aa01161572/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220511%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220511T024352Z&X-Amz-Expires=86400&X-Amz-Signature=7cbc836321e7f1304cac7741220c5b0f14c9fc50b93344d9a8db3ef33ec8e091&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22&x-id=GetObject"} /> {/* 임시이미지--수정해야함 */}
+                                        {/* localhost:3000/이미지주소 -> 개발단계에서 저장된 로컬 이미지 경로는 메타태그에 표시되지 않음 */}
             </Head>
             <PresentComponent />
         </>
     );
 }
 
-// 메타태그 관련 Server Side 처리 // BE에 썸네일, 제목 리턴하는 api 요청해야함
 Present.getInitialProps = async ({query}:any) => {
     let title
     let thumbnail
     await allAxios
-        .get(`/advents/${query.presenturl}`) // 테스트용 임시 요청
+        .get(`/advents/${query.presenturl}/title`)
         .then(({ data }) => {
             title = data.title
-            thumbnail = data.advent_box_list[0].wrapper
+            thumbnail = data.wrapper
         })
     return { title: title, thumbnail: thumbnail };
 };
